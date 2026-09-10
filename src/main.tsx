fdaae3e263d576f759514cd5893f5fd696d6e6bb
Witherstorm <rggrandville@gmail.com>
2026-09-10 10:52:26 +0200

    feat: route clean project URLs to case studies

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { projectDetails } from './data/projectDetails'
+import { ProjectCaseStudy } from './pages/ProjectCaseStudy'
 import './styles/global.css'
 
 const root = document.getElementById('root')
@@ -9,8 +11,15 @@ if (!root) {
   throw new Error('Root element not found')
 }
 
+const normalizedPath = window.location.pathname.replace(/\/$/, '') || '/'
+const project = projectDetails.find((item) => normalizedPath === `/projects/${item.slug}`)
+
+if (project) {
+  document.title = `${project.title} | Regoboth Grandville`
+}
+
 createRoot(root).render(
   <StrictMode>
-    <App />
+    {project ? <ProjectCaseStudy project={project} /> : <App />}
   </StrictMode>,
 )
